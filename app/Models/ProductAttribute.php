bacabd0939fef8c251447f0398b9b5fe976ae783
Isuru <isurujayawickramat@gmail.com>
2023-09-30 21:47:17 +0530

    added product attribute CRUD

---
+++ app/Models/ProductAttribute.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductAttribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'name', 'status'
+    ];
+
+    /**
+     * @param array $input
+     * @return LengthAwarePaginator
+     */
+    final public function getProductAttributes(array $input): LengthAwarePaginator
+    {
+        $query = self::query();
+
+        $per_page = $input['per_page'] ?? 5;
+
+        if (!empty($input['search'])) {
+            $query->where('name', 'like', '%'.$input['search'].'%');
+        }
+
+        if (!empty($input['order_by'])) {
+            $query->orderBy($input['order_by'], $input['direction'] ?? 'asc');
+        }
+
+        return $query
+            ->with('user:id,name')
+            ->paginate($per_page);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LocationDataController;
+use App\Http\Controllers\ProductAttributeController;
 use App\Http\Controllers\SubCategoryController;
 use App\Http\Controllers\SupplierController;
 use Illuminate\Http\Request;
@@ -24,12 +25,13 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], static function () {
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::apiResource('category', CategoryController::class);
+    Route::apiResource('category', CategoryController::class)->except(['create', 'edit']);
     Route::get('categories/list', [CategoryController::class, 'getCategoriesList']);
-    Route::apiResource('sub-category', SubCategoryController::class);
-    Route::apiResource('brand', BrandController::class);
+    Route::apiResource('sub-category', SubCategoryController::class)->except(['create', 'edit']);
+    Route::apiResource('brand', BrandController::class)->except(['create', 'edit']);
     Route::get('provinces', [LocationDataController::class, 'getProvinces']);
     Route::get('districts/{province_id}', [LocationDataController::class, 'getDistrictsByProvinceId']);
     Route::get('cities/{district_id}', [LocationDataController::class, 'getCitiesByDistrictId']);
-    Route::apiResource('supplier', SupplierController::class);
+    Route::apiResource('supplier', SupplierController::class)->except(['create', 'edit']);
+    Route::apiResource('product-attribute', ProductAttributeController::class)->except(['create', 'show', 'edit']);
 });
